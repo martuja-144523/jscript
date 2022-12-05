e670ad7bf02157ebae08753ed3d97d42e6c68a77
martuja-144523 <116412317+martuja-144523@users.noreply.github.com>
2022-12-05 08:04:34 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,24 @@
+var num = 30
+
+if (num >= 10 && num< 18) {
+document.write('Not eligible For Marriege')
+}
+else if ( num >= 18 && num < 22) {
+    document.write('Eligible but it will be good not to do')
+}
+else if( num >=22 && num<=25) {
+    document.write('Eligible but It will be great to wait a little bit')
+} 
+else if(num>25 && num<=30) {
+    document.write('Perfect time to marry If you have a permenent job')
+}
+
+document.write('<br>')
+
+// var digit =19
+// if (digit / 2) {
+//     document.write(Even)
+// }
+// else if (digit = '!='){
+//     document.write(odd)
+// }
